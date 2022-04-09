6f7d9d8f7adabee2e89b5e6ff23cf0a3aee9a976
SandeepK1729 <sandeepkumargalipelly@gmail.com>
2022-04-09 14:00:48 +0530

    added

--- app.py
+++ app.py
@@ -10,3 +10,7 @@ def index():
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/dslab")
+def dslab():
+    return render_template("dslab.html", records = DictReader(open("files/dslab.csv")))
