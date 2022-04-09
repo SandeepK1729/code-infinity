13c454fb29f01429aca793c2a1bb4a5559e33bd7
SandeepK1729 <sandeepkumargalipelly@gmail.com>
2022-04-09 13:37:25 +0530

    added index.html and basic.css

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # return render_template("index.html")
-    return redirect("/login")
+    return render_template("index.html")
+    # return redirect("/login")
 
 @app.route("/login")
 def login():
