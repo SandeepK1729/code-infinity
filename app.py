bf6de060ff2615c49d67ea7a8c36098898077ee1
Konda Sandeep <82932586+SandeepK1729@users.noreply.github.com>
2022-04-15 10:50:06 +0530

    update app.py

--- app.py
+++ app.py
@@ -14,10 +14,13 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+def is_logged_in(): 
+    return session.get("name", "newbie") != "newbie"
+    
 # mail control route
 @app.route("/")
 def index():
-    if not session.get("name") or session["name"] == "newbie":
+    if not is_logged_in():
         return redirect("/login")
         
     return render_template("index.html")
@@ -26,6 +29,7 @@ def index():
 @app.route("/account-verification", methods = ["GET"])
 def verification():
     code = request.args.get("code")
+    
     if Record.verify_mail(code):
         return render_template("verify.html", message = "Your Account Verified, ", link = "/login", word = "login")
     else:
@@ -34,10 +38,11 @@ def verification():
 # login route
 @app.route("/login", methods = ["GET", "POST"])
 def login():
-    if session["name"] == "newbie":
-        return render_template("login.html", message = "Activation link will be sent to your mail ID")
-    elif session["name"]:
-        return redirect("/")
+    if session.get("name"):
+        if session["name"] == "newbie":
+            return render_template("login.html", message = "Activation link will be sent to your mail ID")
+        else:
+            return redirect("/")
 
     global username,pwd, is_valid
     username = request.form.get("username", "default")
@@ -58,7 +63,7 @@ def logout():
 # DS route
 @app.route("/dslab", methods = ["GET", "POST"])
 def dslab():
-    if not session.get("name"):
+    if not is_logged_in():
         return redirect("/login")
     
     return render_template("dslab.html", records = DictReader(open("files/dslab.csv")))    
@@ -66,7 +71,7 @@ def dslab():
 # register route
 @app.route("/register", methods = ["POST", "GET"])
 def register():
-    if session["name"]:
+    if not is_logged_in():
         return redirect("/")
 
     if request.method != "POST":
